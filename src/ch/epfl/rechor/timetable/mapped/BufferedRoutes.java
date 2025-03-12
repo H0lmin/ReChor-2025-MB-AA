4a1259dd3383a6ae6f0faf076335170c995d1ab0
maberrad <Berradamalak222006@gmail.com>
2025-03-12 13:19:40 +0100

    feat(step5): implement BufferedRoutes and BufferedTrips

--- src/ch/epfl/rechor/timetable/mapped/BufferedRoutes.java
+++ src/ch/epfl/rechor/timetable/mapped/BufferedRoutes.java
@@ -1,4 +1,37 @@
 package ch.epfl.rechor.timetable.mapped;
 
-public class BufferedRoutes {
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import ch.epfl.rechor.journey.Vehicle;
+import ch.epfl.rechor.timetable.Routes;
+
+import static ch.epfl.rechor.journey.Vehicle.ALL;
+
+public final class BufferedRoutes implements Routes {
+    private final List<String> stringTable;
+    private final ByteBuffer buffer;
+
+    public BufferedRoutes(List<String> stringTable, ByteBuffer buffer) {
+        this.stringTable = List.copyOf(stringTable);
+        this.buffer = buffer.duplicate().asReadOnlyBuffer();
+    }
+    @Override
+    public Vehicle vehicle(int id){
+        int offset = buffer.getInt(4 + id * 4);
+        byte b = buffer.get(offset);
+        return ALL.get(b);
+    }
+
+    @Override
+    public String name(int id){
+        int offset = buffer.getInt(4 + id * 4);
+        return stringTable.get(offset);
+    }
+
+    @Override
+    public int size(){
+        return stringTable.size();
+    }
+
 }
--- src/ch/epfl/rechor/timetable/mapped/BufferedTrips.java
+++ src/ch/epfl/rechor/timetable/mapped/BufferedTrips.java
@@ -1,4 +1,34 @@
 package ch.epfl.rechor.timetable.mapped;
 
-public class BufferedTrips {
+import java.nio.ByteBuffer;
+import java.util.List;
+
+import ch.epfl.rechor.journey.Vehicle;
+import ch.epfl.rechor.timetable.Routes;
+import ch.epfl.rechor.timetable.Trips;
+
+public class BufferedTrips implements Trips {
+    private final List<String> stringTable;
+    private final ByteBuffer buffer;
+    public BufferedTrips(List<String> stringTable, ByteBuffer buffer) {
+        this.stringTable = List.copyOf(stringTable);
+        this.buffer = buffer.duplicate().asReadOnlyBuffer();
+    }
+
+
+    @Override
+    public int routeId(int id){
+
+    }
+
+    @Override
+    public String destination(int id){
+        int offset = buffer.getInt(4 + id * 4);
+        return stringTable.get(offset);
+    }
+
+    @Override
+    public int size(){
+        return stringTable.size();
+    }
 }
